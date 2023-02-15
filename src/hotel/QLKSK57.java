72d79282f61402ecd01bface11c838e855d3039e
toanvanle123 <125269651+toanvanle123@users.noreply.github.com>
2023-02-15 14:42:02 +0700

    Update QLKSK57.java

--- src/hotel/QLKSK57.java
+++ src/hotel/QLKSK57.java
@@ -1,19 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hotel;
 
 
 public class QLKSK57 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-//        getConnection();
+        
         NewJFrametimkiem newJFrametimkiem = new NewJFrametimkiem();
-//        jf.setSize(600, 500);
-//
+
+
         Dang_Nhap dang_nhap = new Dang_Nhap();
         dang_nhap.getDefaultCloseOperation();
         dang_nhap.setVisible(true);
